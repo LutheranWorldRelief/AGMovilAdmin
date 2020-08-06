94fe26ce42338a67c955460acb59f7621765d3a9
Code Castle El Salvador <info@codecastle.com.sv>
2020-08-06 12:42:30 -0600

    order categories

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CategoriesController < Api::V1::ApplicationController
 
 	def index		
-		@categories = Category.all.order("order ASC").map do |category|
+		@categories = Category.all.order("'order' ASC").map do |category|
 		  { :id => category.id, 
 		  	:name => category.name,
 		  	:image => "https://admin.cacaomovil.com#{category.image.url}",
