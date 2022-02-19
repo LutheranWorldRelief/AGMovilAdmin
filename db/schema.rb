b7fb3a500dc08b567e8e423865228471b42f0833
“Rodolfo <vladcarmona6@gmail.com>
2022-02-19 14:17:46 -0600

    checking if the extension "unaccent" is available in postgresql to use the database

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_02_09_182236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "apps", force: :cascade do |t|
     t.string "name"
