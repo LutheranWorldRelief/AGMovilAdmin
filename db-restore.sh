7070eb36f21d4c34178a88e8468b35e8cc66ccca
“Rodolfo <vladcarmona6@gmail.com>
2022-02-27 14:26:36 -0600

    updated helper script for restore the database from a postgresql script

--- db-restore.sh
+++ db-restore.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+docker-compose exec db bash -c "psql -h localhost -U \${POSTGRES_USER} -c 'DROP DATABASE IF EXISTS '\${DB_NAME}"
 docker-compose exec db bash -c "psql -h localhost -U \${POSTGRES_USER} -c 'CREATE DATABASE '\${DB_NAME}"
-docker-compose exec db bash -c "psql -h localhost -U \${POSTGRES_USER} -d \${DB_NAME} -f /tmp/backup/\${DB_RESTORE_FILE}"
+docker-compose exec db bash -c "psql -h localhost -U \${POSTGRES_USER} -d \${DB_NAME} -f /var/lib/postgresql/backup/\${DB_RESTORE_FILE}"
+docker-compose exec ruby bash -c "rails db:migrate"
