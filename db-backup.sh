4c25004de0c002ff4bfa19b78d4d1160c516cf81
“Rodolfo <vladcarmona6@gmail.com>
2022-02-27 14:25:44 -0600

    updated helper script for backup the database

--- db-backup.sh
+++ db-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec -it db bash -c "pg_dump -U \${POSTGRES_USER} \${DB_NAME} > /tmp/backup/\"\$(date +'%Y%m%d%H%M%S')_\${DB_NAME}.sql\""
+docker-compose exec db bash -c "pg_dump -U \${POSTGRES_USER} \${DB_NAME} > /var/lib/postgresql/backup/\"\$(date +'%Y%m%d_%H%M%S')_\${DB_NAME}.sql\""
